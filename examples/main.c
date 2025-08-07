39e6a74af7d583589f08a596ecda0e55fd8ef094
0x3C-0x3E <rbng0094@gmail.com>
2025-08-07 14:36:54 +0200

    added example

---
+++ examples/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// this is just for consistencys sake
+#include "../src/camp.h"
+
+int main(void) {
+    CampArray* my_arr = camp_array_new(sizeof(int));
+    
+    for (size_t i = 0; i < 100; i++) {
+        camp_array_append(my_arr, &i);
+    }
+
+    camp_array_dump(my_arr);
+
+    camp_array_destroy(my_arr);
+    return 0;
+}
