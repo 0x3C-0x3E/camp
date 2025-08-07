8c83735e58b95025e31be7c57f0185a338c51c0d
0x3C-0x3E <rbng0094@gmail.com>
2025-08-07 16:53:00 +0200

    updated REAMDE docu

--- README.md
+++ README.md
@@ -68,3 +68,22 @@ just like camp_array you init it with:
 ```
 
 > It also supports any type you trow at it
+
+### Possible Functions
+> [!WARNING]
+> still incomplete
+
+``` c
+// inserts as the first element, O(1)
+void camp_ll_insert_at_head(CampLL* list, void* data);
+
+// inserts as the last element, O(1)
+void camp_ll_insert_at_tail(CampLL* list, void* data);
+
+// dumps the linked list
+void camp_ll_dump(CampLL* list);
+
+// returns the data from the node at the given index, O(n)
+void* camp_ll_get_at_index(CampLL* list, size_t index);
+```
+
--- src/camp_linked_list/camp_linked_list.h
+++ src/camp_linked_list/camp_linked_list.h
@@ -26,14 +26,20 @@ typedef struct {
     CampLLNode* tail_node;
 } CampLL;
 
+// returns a ptr to the new linked list
 CampLL* camp_ll_new(size_t data_size);
 
+// inserts as the first element, O(1)
 void camp_ll_insert_at_head(CampLL* list, void* data);
 
+// inserts as the last element, O(1)
 void camp_ll_insert_at_tail(CampLL* list, void* data);
 
+// dumps the linked list
 void camp_ll_dump(CampLL* list);
 
+// returns the data from the node at the given index, O(n)
 void* camp_ll_get_at_index(CampLL* list, size_t index);
 
+// free the list and all the nodes
 void camp_ll_destroy(CampLL* list);
